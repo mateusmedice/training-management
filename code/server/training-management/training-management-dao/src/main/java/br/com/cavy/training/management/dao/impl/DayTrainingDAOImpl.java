76080a8f427fcdab9af19b103470e0cd75c32856
mateusmedice <mateusmedice@192.168.0.16>
2014-11-28 10:36:51 -0200

    Refactoring dao project

--- code/server/training-management/training-management-dao/src/main/java/br/com/cavy/training/management/dao/impl/DayTrainingDAOImpl.java
+++ code/server/training-management/training-management-dao/src/main/java/br/com/cavy/training/management/dao/impl/DayTrainingDAOImpl.java
@@ -2,8 +2,8 @@ package br.com.cavy.training.management.dao.impl;
 
 import org.springframework.stereotype.Repository;
 
+import br.com.cavy.training.management.dao.DayTrainingDAO;
 import br.com.cavy.training.management.model.DayTraining;
-import br.com.progol.training.management.dao.DayTrainingDAO;
 
 @Repository
 public class DayTrainingDAOImpl extends DAOImpl<DayTraining, String> implements DayTrainingDAO {
